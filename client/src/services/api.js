e36500d392204a8cf80c98d203729c3adbb45a3f
Md Sanaul <mdsanaulrahamanedu.com>
2026-09-05 16:09:35 +0530

    api correction

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://construction-application-umyp.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL || 'https://construction-application-9w5j.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
